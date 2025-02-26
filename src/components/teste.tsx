ea9216baef095fa6e297a6257eb64fd184286a7a
Gabriel Luiz <gabriellnsilva2004@gmail.com>
2025-02-25 21:45:45 -0300

    Adição de Arquivos

---
+++ src/components/teste.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Teste = () => {
+    return(
+        <div>
+            <img src="/img/banguela.gif" className="App-logo" alt="Banguela" />
+            <p>
+            Insira a música de Driftveil city aqui
+            </p>
+            <p>
+            Por quê você está aqui?
+            </p>
+        </div> 
+    )
+};
+
+export default Teste;
